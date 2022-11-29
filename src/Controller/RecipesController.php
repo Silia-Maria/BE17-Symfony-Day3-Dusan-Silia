180c4905822df2460108dce7544f3f5ef85ba0cc
Silia Cronauer <silia.maria@web.de>
2022-11-29 13:07:51 +0100

    added index & delete

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -2,17 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipes;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class RecipesController extends AbstractController
 {
+    # Index -> homePage
     #[Route('/recipes', name: 'app_recipes')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $recipes = $doctrine->getRepository(Recipes::class)->findAll();
         return $this->render('recipes/index.html.twig', [
-            'controller_name' => 'RecipesController',
+            'recipes' => $recipes,
         ]);
+    }
+
+    # Create
+    #[Route('/create', name: 'create')]
+    public function create(): Response
+    {
+        return $this->render('recipes/create.html.twig', []);
+    }
+
+    # Edit
+    #[Route('/edit/{id}', name: 'edit')]
+    public function edit($id): Response
+    {
+        return $this->render('recipes/edit.html.twig', []);
+    }
+
+    # Details
+    #[Route('/details/{id}', name: 'details')]
+    public function details($id): Response
+    {
+        return $this->render('recipes/index.html.twig', []);
+    }
+
+    # Delete
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete($id, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $recipe = $doctrine->getRepository(Recipes::class)->find($id);
+        $em->remove($recipe);
+        $em->flush();
+        return $this->redirectToRoute('index');
     }
 }
---
+++ src/Entity/Recipes.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipesRepository::class)]
+class Recipes
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?int $serves = null;
+
+    #[ORM\Column]
+    private ?int $time = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $createDate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $difficulty = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $link = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getServes(): ?int
+    {
+        return $this->serves;
+    }
+
+    public function setServes(int $serves): self
+    {
+        $this->serves = $serves;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getCreateDate(): ?\DateTimeInterface
+    {
+        return $this->createDate;
+    }
+
+    public function setCreateDate(\DateTimeInterface $createDate): self
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?string
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(string $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
